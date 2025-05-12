6fdddc410ed26f13a4b7e10d5702365a697de6db
snakeworks <filipsmolcec@gmail.com>
2025-05-12 23:01:14 +0200

    Add simple player turn GUI animations

---
+++ Assets/Scripts/Board/BoardGUIAnimations.cs
@@ -0,0 +1,38 @@
+using System.Threading.Tasks;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+
+public class BoardGUIAnimations : StaticInstance<BoardGUIAnimations>
+{
+    [SerializeField] private TextMeshProUGUI _playerTurnText;
+    [SerializeField] private CanvasGroup _playerTurnCanvasGroup;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _playerTurnCanvasGroup.gameObject.SetActive(false);
+    }
+
+    public async Task AnimatePlayerTurn()
+    {
+        Sequence sequence = DOTween.Sequence();
+
+        _playerTurnCanvasGroup.gameObject.SetActive(true);
+        _playerTurnCanvasGroup.alpha = 0.0f;
+        var rectTransform = _playerTurnCanvasGroup.GetComponent<RectTransform>();
+        rectTransform.DOAnchorPosY(-50.0f, 0.0f);
+
+        var currentPlayer = BoardManager.Instance.CurrentPlayer;
+        _playerTurnText.SetText($"PLAYER {currentPlayer.Index+1} TURN");
+
+        sequence.Insert(0.0f, rectTransform.DOAnchorPosY(0.0f, 0.15f));
+        sequence.Insert(0.0f, _playerTurnCanvasGroup.DOFade(1.0f, 0.15f));
+        sequence.Insert(1.5f, rectTransform.DOAnchorPosY(-50.0f, 0.0f));
+        sequence.Insert(1.5f, _playerTurnCanvasGroup.DOFade(0.0f, 0.15f));
+
+        await sequence.AsyncWaitForCompletion();
+
+        _playerTurnCanvasGroup.gameObject.SetActive(false);
+    }
+}
--- Assets/Scripts/States/ChoosingBoardActionState.cs
+++ Assets/Scripts/States/ChoosingBoardActionState.cs
@@ -4,16 +4,19 @@ public class ChoosingBoardActionState : GameState
 {
     public override async void OnEnter()
     {
-        PlayerManager.Instance.EnableInput();
-        PlayerManager.Instance.GiveOwnershipTo(CurrentController);
         BoardCamera.Instance.TransitionToPlayer(CurrentBoardPlayerController.Index);
-    
+        
         while (BoardCamera.Instance.IsTransitioning)
         {
             await Task.Yield();
         }
-
+     
+        await BoardGUIAnimations.Instance.AnimatePlayerTurn();
+        
         await Task.Delay(250);
+
+        PlayerManager.Instance.EnableInput();
+        PlayerManager.Instance.GiveOwnershipTo(CurrentController);
 
         BoardManager.Instance.BoardPlayerActionMenu.OnDiceRollPressed += OnDiceRollPressed;
         BoardManager.Instance.BoardPlayerActionMenu.PushWithBoardPlayerData(CurrentPlayerData);
